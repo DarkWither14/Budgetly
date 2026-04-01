b9a2fb84a7042a1af1ae3b978123a4aa7f836cd9
Michael Joseph Wagner <mjwagn3@ilstu.edu>
2026-04-01 17:08:23 -0500

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,15 @@
+public class Account{
+    private int accountID;
+    private String email;
+    private int passwordHash;
+    private List<Profile> profileList;
+    public Account(){
+       profileList = new ArrayList<>();   
+    }
+    public void addProfileToList(String name){
+        if(profileList.size()<3){
+          
+        }
+    }
+  
+    }
