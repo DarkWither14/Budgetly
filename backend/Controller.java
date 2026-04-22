1c329a8f4d318f1445cbf2b5b0b0ba62f0452d2d
Daniel Opperman <DarkWither14@yahoo.com>
2026-04-22 16:57:09 -0500

    Cleanup: remove duplicate files and minor code review

--- backend/Controller.java
+++ backend/Controller.java
@@ -50,7 +50,6 @@ public class Controller {
 
     public int getActiveProfileId() { return activeProfileId; }
 
-    VerifyData getVerifyData() { return verifyData; }
 
     public boolean hasActiveTransactionGroup() {
         return transOperations.getTransactionGroup() != null;
